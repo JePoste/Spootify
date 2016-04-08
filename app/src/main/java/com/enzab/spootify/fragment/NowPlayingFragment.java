06767de4a99733722aaf92602cf2ac9db1041636
Enzo De Gregorio <jeposte@gmail.com>
2016-04-07 23:15:56 -0700

    + Added media button images to the Now Playing view. + Added sound playing/pausing when pressing Play/Pause media button

--- app/src/main/java/com/enzab/spootify/fragment/NowPlayingFragment.java
+++ app/src/main/java/com/enzab/spootify/fragment/NowPlayingFragment.java
@@ -1,22 +1,97 @@
 package com.enzab.spootify.fragment;
 
+import android.graphics.BitmapFactory;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.enzab.spootify.R;
+import com.squareup.picasso.Picasso;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class NowPlayingFragment extends Fragment {
+
+    static double PLAY_BUTTON_RATIO = 1.5;
+    static int playButtonWidth;
+    static int playButtonHeight;
+
+    MediaPlayer music;
+
+    @Bind(R.id.album_cover)
+    ImageView albumCover;
+    @Bind(R.id.option_button)
+    ImageView optionButton;
+    @Bind(R.id.play_button)
+    ImageView playButton;
+    @Bind(R.id.previous_button)
+    ImageView previousButton;
+    @Bind(R.id.next_button)
+    ImageView nextButton;
+    @Bind(R.id.shuffle_button)
+    ImageView shuffleButton;
+    @Bind(R.id.repeat_button)
+    ImageView repeatButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        BitmapFactory.Options dimensions = new BitmapFactory.Options();
+        dimensions.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), R.mipmap.ic_play_circle_outline_white_48dp, dimensions);
+        playButtonWidth = (int) (dimensions.outWidth * PLAY_BUTTON_RATIO);
+        playButtonHeight = (int) (dimensions.outHeight * PLAY_BUTTON_RATIO);
+
+        music = MediaPlayer.create(getContext(), R.raw.strauss_also_sprach_zarathustra);
+        music.setOnCompletionListener(soundCompletionListener);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_now_playing, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_now_playing, container, false);
+        ButterKnife.bind(this, view);
+
+        playButton.setTag(R.mipmap.ic_play_circle_outline_white_48dp);
+        Picasso.with(getContext()).load(R.mipmap.ic_play_circle_outline_white_48dp).resize(playButtonWidth, playButtonHeight).noFade().into(playButton);
+        Picasso.with(getContext()).load(R.drawable.cover1).into(albumCover);
+        Picasso.with(getContext()).load(R.mipmap.ic_more_vert_white_36dp).into(optionButton);
+        Picasso.with(getContext()).load(R.mipmap.ic_skip_previous_white_36dp).into(previousButton);
+        Picasso.with(getContext()).load(R.mipmap.ic_skip_next_white_36dp).into(nextButton);
+        Picasso.with(getContext()).load(R.mipmap.ic_shuffle_white_36dp).into(shuffleButton);
+        Picasso.with(getContext()).load(R.mipmap.ic_repeat_white_36dp).into(repeatButton);
+
+        return view;
     }
+
+    @OnClick(R.id.play_button)
+    public void playButtonPressed(View view) {
+        if (String.valueOf(playButton.getTag()).equals(String.valueOf(R.mipmap.ic_play_circle_outline_white_48dp))) {
+            music.start();
+            playButton.setTag(R.mipmap.ic_pause_circle_outline_white_36dp);
+            Picasso.with(getContext()).load(R.mipmap.ic_pause_circle_outline_white_36dp).resize(playButtonWidth, playButtonHeight).noFade().into(playButton);
+        } else {
+            music.pause();
+            playButton.setTag(R.mipmap.ic_play_circle_outline_white_48dp);
+            Picasso.with(getContext()).load(R.mipmap.ic_play_circle_outline_white_48dp).resize(playButtonWidth, playButtonHeight).noFade().into(playButton);
+        }
+    }
+
+    MediaPlayer.OnCompletionListener soundCompletionListener = new MediaPlayer.OnCompletionListener() {
+        public void onCompletion(MediaPlayer sound) {
+            sound.release();
+        }
+    };
 }
